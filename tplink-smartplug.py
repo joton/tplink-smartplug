b5fa995eadea4a484bc400ec8755d0fb289fd0b5
joaquinoton <joton@cnb.csic.es>
2017-07-01 20:55:58 +0200

    Added hostname and variable port support

--- tplink-smartplug.py
+++ tplink-smartplug.py
@@ -24,76 +24,88 @@ import argparse
 
 version = 0.1
 
+
 # Check if IP is valid
 def validIP(ip):
-	try:
-		socket.inet_pton(socket.AF_INET, ip)
-	except socket.error:
-		parser.error("Invalid IP Address.")
-	return ip 
+    try:
+        socket.inet_pton(socket.AF_INET, ip)
+    except socket.error:
+        parser.error("Invalid IP Address.")
+    return ip
+
 
 # Predefined Smart Plug Commands
 # For a full list of commands, consult tplink_commands.txt
-commands = {'info'     : '{"system":{"get_sysinfo":{}}}',
-			'on'       : '{"system":{"set_relay_state":{"state":1}}}',
-			'off'      : '{"system":{"set_relay_state":{"state":0}}}',
-			'cloudinfo': '{"cnCloud":{"get_info":{}}}',
-			'wlanscan' : '{"netif":{"get_scaninfo":{"refresh":0}}}',
-			'time'     : '{"time":{"get_time":{}}}',
-			'schedule' : '{"schedule":{"get_rules":{}}}',
-			'countdown': '{"count_down":{"get_rules":{}}}',
-			'antitheft': '{"anti_theft":{"get_rules":{}}}',
-			'reboot'   : '{"system":{"reboot":{"delay":1}}}',
-			'reset'    : '{"system":{"reset":{"delay":1}}}'
-}
+commands = {'info': '{"system":{"get_sysinfo":{}}}',
+            'on': '{"system":{"set_relay_state":{"state":1}}}',
+            'off': '{"system":{"set_relay_state":{"state":0}}}',
+            'cloudinfo': '{"cnCloud":{"get_info":{}}}',
+            'wlanscan': '{"netif":{"get_scaninfo":{"refresh":0}}}',
+            'time': '{"time":{"get_time":{}}}',
+            'schedule': '{"schedule":{"get_rules":{}}}',
+            'countdown': '{"count_down":{"get_rules":{}}}',
+            'antitheft': '{"anti_theft":{"get_rules":{}}}',
+            'reboot': '{"system":{"reboot":{"delay":1}}}',
+            'reset': '{"system":{"reset":{"delay":1}}}'
+            }
+
 
 # Encryption and Decryption of TP-Link Smart Home Protocol
 # XOR Autokey Cipher with starting key = 171
 def encrypt(string):
-	key = 171
-	result = "\0\0\0\0"
-	for i in string: 
-		a = key ^ ord(i)
-		key = a
-		result += chr(a)
-	return result
+    key = 171
+    result = "\0\0\0\0"
+    for i in string:
+        a = key ^ ord(i)
+        key = a
+        result += chr(a)
+    return result
+
 
 def decrypt(string):
-	key = 171 
-	result = ""
-	for i in string: 
-		a = key ^ ord(i)
-		key = ord(i) 
-		result += chr(a)
-	return result
+    key = 171
+    result = ""
+    for i in string:
+        a = key ^ ord(i)
+        key = ord(i)
+        result += chr(a)
+    return result
+
 
 # Parse commandline arguments
 parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v" + str(version))
-parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
+# parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
+parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address")
+parser.add_argument("-p", "--port", metavar="<port>", required=False, default=9999, type=int, help="Port")
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-c", "--command", metavar="<command>", help="Preset command to send. Choices are: "+", ".join(commands), choices=commands) 
+group.add_argument("-c", "--command", metavar="<command>",
+                   help="Preset command to send. Choices are: " + ", ".join(commands), choices=commands)
 group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
 args = parser.parse_args()
 
 # Set target IP, port and command to send
-ip = args.target
-port = 9999
-if args.command is None:
-	cmd = args.json
-else:
-	cmd = commands[args.command]
-
-
-
-# Send command and receive reply 
 try:
-	sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock_tcp.connect((ip, port))
-	sock_tcp.send(encrypt(cmd))
-	data = sock_tcp.recv(2048)
-	sock_tcp.close()
-	
-	print "Sent:     ", cmd
-	print "Received: ", decrypt(data[4:])
+    socket.inet_aton(args.target)
+    ip = args.target
 except socket.error:
-	quit("Cound not connect to host " + ip + ":" + str(port))
+    ip = socket.gethostbyname(args.target)
+
+port = args.port
+
+if args.command is None:
+    cmd = args.json
+else:
+    cmd = commands[args.command]
+
+# Send command and receive reply
+try:
+    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_tcp.connect((ip, port))
+    sock_tcp.send(encrypt(cmd))
+    data = sock_tcp.recv(2048)
+    sock_tcp.close()
+
+    print "Sent:     ", cmd
+    print "Received: ", decrypt(data[4:])
+except socket.error:
+    quit("Cound not connect to host " + ip + ":" + str(port))
